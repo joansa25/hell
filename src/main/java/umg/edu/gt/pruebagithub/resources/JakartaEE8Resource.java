870c402cd4a7e21cacd9f74be32b9fc54f56d2f4
joansaan23 <joansaan23@gmail.com>
2024-04-16 01:08:48 -0600

    se agrego el proyecto

---
+++ src/main/java/umg/edu/gt/pruebagithub/resources/JakartaEE8Resource.java
@@ -0,0 +1,20 @@
+package umg.edu.gt.pruebagithub.resources;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.core.Response;
+
+/**
+ *
+ * @author 
+ */
+@Path("rest")
+public class JakartaEE8Resource {
+    
+    @GET
+    public Response ping(){
+        return Response
+                .ok("ping")
+                .build();
+    }
+}
